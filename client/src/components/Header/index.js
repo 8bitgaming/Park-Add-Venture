c1e14b1eccd5c90e79fa89aba0858d0b2e64be1d
hjohnsick <hjohnsick@yahoo.com>
2022-05-01 13:50:07 -0400

    fixed homepage route

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,26 +1,25 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../../images/logo.png";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar className="header" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to="/">
           <img src={logo} alt="Park-Add-Venture logo" className="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="header-links" href="#link">
+            <Nav.Link className="header-links" as={Link} to="/">
               Parks
             </Nav.Link>
             <Nav.Link className="header-links" href="#link">
               Profile
             </Nav.Link>
-            <Nav.Link className="header-links" href="#link">
-              My Parks
-            </Nav.Link>
+            <Nav.Link className="header-links">My Parks</Nav.Link>
             <Nav.Link className="header-links" href="#link">
               Get Pass
             </Nav.Link>
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -3,8 +3,7 @@ import { useEffect, useState } from "react";
 
 const Homepage = () => {
   const [parks, setParks] = useState([]);
-  const url =
-    "https://developer.nps.gov/api/v1/parks?&limit=466&api_key=osna9h6fzGHW7xBfKZcSmlO2JuN61g4jK1mlHH5u";
+  const url = `https://developer.nps.gov/api/v1/parks?&limit=466&api_key=${process.env.REACT_APP_API_KEY}`;
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -12,12 +11,12 @@ const Homepage = () => {
         console.log(data);
         setParks(data.data);
       });
-  }, []);
+  }, [url]);
   const nationalParks = parks.filter(
     (park) => park.designation === "National Park"
   );
   return (
-    <div className="App">
+    <div className="homepage">
       {nationalParks.map((park) => (
         <ParkCard key={park.id} {...park} />
       ))}
