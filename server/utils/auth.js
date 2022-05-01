78e54ccfa059621ee46a1f15b2c38141d5b9f54e
dwetoskey <dwetoskey224@gmail.com>
2022-04-30 23:53:00 -0400

    added jwt secret to .env file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
-const secret = 'scalamoushhhhhh';
 const expiration = '2h';
 
 module.exports = {
@@ -21,7 +22,7 @@ module.exports = {
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, JWT_SECRET, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
@@ -32,6 +33,6 @@ module.exports = {
   signToken: function({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, JWT_SECRET, { expiresIn: expiration });
   }
 };
