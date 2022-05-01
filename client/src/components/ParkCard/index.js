db1386a3059a918efb3bf364c930601fb619c943
hjohnsick <hjohnsick@yahoo.com>
2022-05-01 19:47:34 -0400

    added homepage

--- client/src/components/ParkCard/index.js
+++ client/src/components/ParkCard/index.js
@@ -1,25 +1,38 @@
-import { Card } from "react-bootstrap";
+import { Card, Accordion, Col } from "react-bootstrap";
 
 const ParkCard = ({ description, name, states, url, images }) => {
   return (
-    <div>
+    <Col className="container-fluid d-flex justify-content-center">
       <Card style={{ width: "18rem" }}>
         <Card.Img
           variant="top"
           src={images[0].url}
-          style={{ width: "18rem" }}
+          style={{ width: "17.9rem", height: "14rem" }}
         />
-        <Card.Body>
-          <Card.Title>
-            {name}, {states}{" "}
-          </Card.Title>
-          <Card.Text>{description}</Card.Text>
-          <Card.Link href={url} target="_blank" rel="noopener noreferrer">
-            Learn More
-          </Card.Link>
+        <Card.Body className="yellow-background">
+          <Accordion flush>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header className="yellow-background">
+                {" "}
+                {name}, {states}
+              </Accordion.Header>
+              <Accordion.Body className="text-left">
+                {description}
+                <Card.Body className="text-center">
+                  <Card.Link
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn More
+                  </Card.Link>
+                </Card.Body>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Card.Body>
       </Card>
-    </div>
+    </Col>
   );
 };
 
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,5 +1,7 @@
 import ParkCard from "../components/ParkCard";
 import { useEffect, useState } from "react";
+import "./homepage.css";
+import { Row } from "react-bootstrap";
 
 const Homepage = () => {
   const [parks, setParks] = useState([]);
@@ -17,9 +19,28 @@ const Homepage = () => {
   );
   return (
     <div className="homepage">
-      {nationalParks.map((park) => (
-        <ParkCard key={park.id} {...park} />
-      ))}
+      <div className="text-center pb-5">
+        <h1 className="pt-5">U.S. National Parks</h1>
+        <p className="mobile">
+          Explore the many exciting National Parks and Monuments of the United
+          States. Select the name for more information, or use the Visit! button
+          to add to a list of parks you would like to add to a personal list to
+          visit in the future.
+        </p>
+        <p className="desktop">
+          Explore the many exciting National Parks and Monuments of the United
+          States.
+          <br /> Select the name for more information, or use the Visit! button
+          to add to a list of parks you would like to add to a personal list to
+          visit in the future.
+        </p>
+      </div>
+
+      <Row xs={1} md={3} lg={4} xxl={5} className="g-4 card-container">
+        {nationalParks.map((park) => (
+          <ParkCard key={park.id} {...park} />
+        ))}
+      </Row>
     </div>
   );
 };
