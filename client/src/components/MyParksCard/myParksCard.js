23c79123c5c9e14059253e06f2503cf2b16a7bf1
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-05-02 01:30:04 -0500

    graphql query, page and component relationship, connected to nav, added custom stamp

--- client/src/components/MyParksCard/myParksCard.js
+++ client/src/components/MyParksCard/myParksCard.js
@@ -3,8 +3,9 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import { Container, Row, Col } from "react-bootstrap";
+import visitedStamp from '../../images/visitedStampRotated.png'
 
-const myParks = ({}) => {
+const MyParksCard = ({}) => {
   //   if (!id.length) return <h3>No Parks Saved or Visited</h3>;
 
   return (
@@ -12,17 +13,13 @@ const myParks = ({}) => {
         <Row>
             <Col>
       <Card style={{ width: "18rem"}}>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
+        <Card.Body className="yellow-background">
           <Card.Title>Yellowstone</Card.Title>
-          <Card.Text>Lorem ipsem</Card.Text>
+          <Card.Img variant="top" src={visitedStamp} style={{ width: "5rem", height: "5rem" }} />
+          <Card.Text>Visited!</Card.Text>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>Visited 8/12/2020</ListGroup.Item>
-        </ListGroup>
         <Card.Body>
-          <Button variant="primary">Primary</Button>{" "}
-          <Button variant="secondary">Secondary</Button>{" "}
+          <Button variant="primary">Edit</Button>{" "}
         </Card.Body>
       </Card>
       </Col>
@@ -31,4 +28,4 @@ const myParks = ({}) => {
   );
 };
 
-export default myParks;
+export default MyParksCard;
--- client/src/pages/myParks.js
+++ client/src/pages/myParks.js
@@ -1,45 +1,18 @@
-// import React from 'react';
-// import myParks from '../components/MyParks/myParks';
+import React from "react";
+import MyParksCard from "../components/MyParksCard/myParksCard";
 
-// import Auth from '../utils/auth';
-// import { useQuery } from '@apollo/client';
-// import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+// import { QUERY_USER_PARKS } from '../utils/queries';
 
-// const MyParks = () => {
-//     const { loading, data } = useQuery(QUERY_THOUGHTS);
-//     const { data: userData } = useQuery(QUERY_ME_BASIC);
-//     const thoughts = data?.thoughts || [];
-  
-//     const loggedIn = Auth.loggedIn();
-  
-//     return (
-//       <main>
-//         <div className="flex-row justify-space-between">
-//           {loggedIn && (
-//             <div className="col-12 mb-3">
-//               <ThoughtForm />
-//             </div>
-//           )}
-//           <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-//             {loading ? (
-//               <div>Loading...</div>
-//             ) : (
-//               <ThoughtList
-//                 thoughts={thoughts}
-//                 title="Some Feed for Thought(s)..."
-//               />
-//             )}
-//           </div>
-//           {loggedIn && userData ? (
-//             <div className="col-12 col-lg-3 mb-3">
-//               <FriendList
-//                 username={userData.me.username}
-//                 friendCount={userData.me.friendCount}
-//                 friends={userData.me.friends}
-//               />
-//             </div>
-//           ) : null}
-//         </div>
-//       </main>
-//     );
-//   };
+const MyParks = () => {
+//   const { loading, data } = useQuery(QUERY_USER_PARKS);
+//   const user = data?.user || [];
+  const loggedIn = Auth.loggedIn();
+
+  return (
+    <MyParksCard />
+  );
+};
+
+export default MyParks;
