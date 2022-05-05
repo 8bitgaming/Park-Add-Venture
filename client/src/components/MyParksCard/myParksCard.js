b366cd01046ef8679cbf12ffb973ddd83a400118
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-05-05 01:03:05 -0500

    starting to add visit

--- client/src/components/MyParksCard/myParksCard.js
+++ client/src/components/MyParksCard/myParksCard.js
@@ -1,48 +1,60 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Container, Row, Col } from "react-bootstrap";
 import visitedStamp from "../../images/visitedStampRotated.png";
 import "./myParksCard.css";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER_PARKS } from "../utils/queries";
 
 const MyParksCard = ({ parkName, image, link, visited, dateVisited }) => {
-  console.log("parkname", parkName)
-  //   if (!id.length) return <h3>No Parks Saved or Visited</h3>;
 
-  //need to add ternary to switch between visited or not visited
+  // useEffect(() => {
+  //   async function getCart() {
+  //     const cart = await idbPromise('cart', 'get');
+  //     dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+  //   }
+
+  //   if (!state.cart.length) {
+  //     getCart();
+  //   }
+  // }, [state.cart.length, dispatch]);
+
   return (
-    <Container className="container-fluid d-flex justify-content-center mt-10 my-parks-page">
-      <Row>
-        <Col>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src={image}
-              style={{ width: "17.9rem", height: "14rem" }}
-            />
-            <Card.Body className="yellow-background">
-              <Card.Title className="text-center">{parkName}</Card.Title>
-              <Card.Img
+    <>
+      {!parkName ? (
+        <Container className="container-fluid d-flex justify-content-center mt-10">
+          No Parks Saved or Visited! Use the Parks page to select some.
+        </Container>
+      ) : (
+        <Container className="container-fluid d-flex justify-content-center mt-10 my-parks-page">
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={image}
+                  style={{ width: "17.9rem", height: "14rem" }}
+                />
+                <Card.Body className="yellow-background">
+                  <Card.Title className="text-center">{parkName}</Card.Title>
+                  {visited && (
+                    <Card.Img
                       className="stamp"
                       src={visitedStamp}
                       style={{ width: "6rem", height: "6rem" }}
                     />
-              <Container>
-                <Row>
-                  <Col>
-                    <Card.Text>Visited on 7/25/2021</Card.Text>
-                  </Col>
-                  <Col>
-                  <Button variant="success">Edit</Button>{" "}
-                  </Col>
-                </Row>
-              </Container>
-
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+                  )}
+                  <Container className="container-fluid d-flex justify-content-center mt-10">
+                    <Button variant="success">Visited!</Button>{" "}
+                  </Container>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </>
   );
 };
 
