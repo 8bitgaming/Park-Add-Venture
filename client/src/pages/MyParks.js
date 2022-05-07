45d68a40189ace20c964ae28d335f1c88891b529
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-05-07 16:59:24 -0500

    added message for when no parks are added to mylist

--- client/src/pages/MyParks.js
+++ client/src/pages/MyParks.js
@@ -1,40 +1,46 @@
 import Row from "react-bootstrap/Row";
 import MyParksCard from "../components/MyParksCard/myParksCard";
 import Auth from "../utils/auth";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { QUERY_USER_PARKS } from "../utils/queries";
 import "../components/MyParksCard/myParksCard.css";
 import React, { useEffect, useState } from "react";
-
+import Container from "react-bootstrap/Container";
 
 const MyParks = () => {
   const loggedIn = Auth.loggedIn();
   const { loading, data, refetch } = useQuery(QUERY_USER_PARKS);
   const user = data?.me || [];
-  const [triggerLoading, setTriggerLoading] = useState(user)
+  const [triggerLoading, setTriggerLoading] = useState(user);
 
   useEffect(() => {
-    console.log("useEffect from MyParks Triggered", triggerLoading)
-    refetch()
-
+    refetch();
   }, [triggerLoading]);
 
   if (loading) {
     return <div>Page is loading.</div>;
   }
 
-  return (
+  return !user.length ? (
+    <Container>No parks added. Use the parks page to add some parks to visit!</Container>
+  ) : (
     loggedIn && (
-        <Row xs={1} md={3} lg={4} xxl={5} className="g-4 card-container my-parks-page">
-          {user.savedParks.map((park) => (
-            <MyParksCard
-              key={park.parkId}
-              {...park}
-              setTriggerLoading={setTriggerLoading}
-              triggerLoading={triggerLoading}
-            />
-          ))}
-        </Row>
+      <Row
+        xs={1}
+        md={3}
+        lg={4}
+        xxl={5}
+        className="g-4 card-container my-parks-page"
+      >
+        {user.savedParks.map((park) => (
+          <MyParksCard
+            key={park.parkId}
+            {...park}
+            setTriggerLoading={setTriggerLoading}
+            triggerLoading={triggerLoading}
+          />
+        ))}
+      </Row>
     )
   );
 };
