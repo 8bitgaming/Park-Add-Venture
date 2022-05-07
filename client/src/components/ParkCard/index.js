369d5fff4ba917cb3ff64a11c6fbd4da252538ed
hjohnsick <hjohnsick@yahoo.com>
2022-05-07 10:23:39 -0400

    added delete button

--- client/src/components/ParkCard/index.js
+++ client/src/components/ParkCard/index.js
@@ -1,4 +1,4 @@
-import { Card, Accordion, Col, Container, Row,} from "react-bootstrap";
+import { Card, Accordion, Col, Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
@@ -10,19 +10,20 @@ import { useQuery } from "@apollo/client";
 
 const ParkCard = ({ description, name, states, url, images, id }) => {
   const loggedIn = Auth.loggedIn();
-  const [parkAdded, setParkAdded] = useState(false);
   const { data } = useQuery(QUERY_USER_PARKS);
   const user = data?.me || [];
+  const [parkAdded, setParkAdded] = useState(false);
+
   // console.log(`These should be the saved parks ${data.me.savedParks.parkId}`);
   // user.savedParks.map((park) => {
   //   console.log(`These are the saved parks ${park.parkId}`);
   // });
 
-  useEffect(() => {
-    user.savedParks.map((park) =>
-      park.parkId === id ? setParkAdded(true) : setParkAdded(false)
-    );
-  }, []);
+  // useEffect(() => {
+  //   user.savedParks.map((park) =>
+  //     park.parkId === id ? setParkAdded(true) : setParkAdded(false)
+  //   );
+  // }, []);
 
   // const [parkAdded, setParkAdded] = useState(() => {
   //   // getting stored value
@@ -78,15 +79,15 @@ const ParkCard = ({ description, name, states, url, images, id }) => {
     },
   });
 
-  const deletePark = (event) => {
-    removePark({
+  const deletePark = async (event) => {
+    await removePark({
       variables: { parkId: id },
     });
     setParkAdded(false);
   };
 
-  const addPark = (event) => {
-    savePark({
+  const addPark = async (event) => {
+    await savePark({
       variables: {
         parkId: id,
         parkName: name,
@@ -102,56 +103,59 @@ const ParkCard = ({ description, name, states, url, images, id }) => {
   return (
     <Container>
       <Row>
-    <Col className="container-fluid d-flex justify-content-center">
-      <Card style={{ width: "18rem" }}>
-        <div className="image-container">
-          <Card.Img
-            variant="top"
-            src={images[0].url}
-            style={{ width: "17.9rem", height: "14rem" }}
-          />
-          {loggedIn && (
-            <>
-              {!parkAdded ? (
-                <button className="btn btn-add btn-no-shadow" onClick={addPark}>
-                  <FontAwesomeIcon icon={faCirclePlus} size="2x" />
-                </button>
-              ) : (
-                <button
-                  className="btn btn-remove btn-no-shadow"
-                  onClick={deletePark}
-                >
-                  <FontAwesomeIcon icon={faCircleCheck} size="2x" />
-                </button>
+        <Col className="container-fluid d-flex justify-content-center">
+          <Card style={{ width: "18rem" }}>
+            <div className="image-container">
+              <Card.Img
+                variant="top"
+                src={images[0].url}
+                style={{ width: "17.9rem", height: "14rem" }}
+              />
+              {loggedIn && (
+                <>
+                  {!parkAdded ? (
+                    <button
+                      className="btn btn-add btn-no-shadow"
+                      onClick={addPark}
+                    >
+                      <FontAwesomeIcon icon={faCirclePlus} size="2x" />
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-remove btn-no-shadow"
+                      onClick={deletePark}
+                    >
+                      <FontAwesomeIcon icon={faCircleCheck} size="2x" />
+                    </button>
+                  )}
+                </>
               )}
-            </>
-          )}
-        </div>
-        <Card.Body className="yellow-background">
-          <Accordion flush>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header className="yellow-background">
-                {" "}
-                {name}, {states}
-              </Accordion.Header>
-              <Accordion.Body className="text-left">
-                {description}
-                <Card.Body className="text-center">
-                  <Card.Link
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn More
-                  </Card.Link>
-                </Card.Body>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Card.Body>
-      </Card>
-    </Col>
-    </Row>
+            </div>
+            <Card.Body className="yellow-background">
+              <Accordion flush>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header className="yellow-background">
+                    {" "}
+                    {name}, {states}
+                  </Accordion.Header>
+                  <Accordion.Body className="text-left">
+                    {description}
+                    <Card.Body className="text-center">
+                      <Card.Link
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Learn More
+                      </Card.Link>
+                    </Card.Body>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
